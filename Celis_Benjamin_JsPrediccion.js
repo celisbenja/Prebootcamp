62612077a7b4b848995003c07af6d5e1afb6fea1
celisbenja <acevedobenjamin32@gmail.com>
2022-09-30 10:57:58 -0300

    ok

---
+++ Celis_Benjamin_JsPrediccion.js
@@ -0,0 +1,21 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+// console.log imprime Naci en 1980   
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+//console.log imprime Naci en 1980
+
+function add(num1, num2){
+        console.log("¡Sumando números!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    
+/*console.log imprime sumando numeros
+console.log imprime 10
+console.log imprime 20 
+console.log imprime el resultado 30*/    
