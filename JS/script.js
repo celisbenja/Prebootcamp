b016e9b6c26c6a73b524464b973952c22403e5c9
benjamin celis acevedo <acevedobenjamin32@gmail.com>
2023-03-17 11:09:50 -0300

    ok

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function mensaje(element) {
+    console.log("elemento clickeado", element);
+    alert(' akmlnhuujgsisuhnhyusfhnjjisghsggimjs<shnlshnu<gjhnuhn')
+}
+function hide(element) {
+    element.remove();
+}
+function turnOff(element) {
+    element.innerText = "Login";
+}
