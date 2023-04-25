0cb8d794abe19fbec3db648dd454c2d0a21f991d
benjamin celis acevedo <acevedobenjamin32@gmail.com>
2023-04-25 08:32:30 -0400

    ok

---
+++ Antiguo/ICCP/diagram.js
@@ -0,0 +1,7 @@
+var nombre = prompt ("ingresar nombre");
+var apellido = prompt ("ingresar apellido");
+var edad = prompt ("ingresar edad");
+console.log ("mis datos son: " + nombre + " " + apellido + " " + edad );
+
+
+var numero = prompt ("ingresar numero");
